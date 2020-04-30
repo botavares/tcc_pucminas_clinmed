41a9a5290a8e56ff565fb47f94d0d54c30f294a6
botavares <42076691+botavares@users.noreply.github.com>
2020-04-30 15:43:18 -0300

    PUC MINAS
    Breno Oliveira Tavares

--- codigoFonte/clinMed/application/views/layoutLogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-//carrega a view com o cabeçalho//
-$this->load->view('fixos/header');
-
-$this->load->view($camada1 .'/'.$camada2 . '/' . $pagina.'_view');
-
-$this->load->view('fixos/footer');
-?>   
